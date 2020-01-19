375f21ab11e56daf20c00f9d13a35eef18b5e621
Jannik Emmerich <mail@jannik-emmerich.de>
2020-01-19 13:20:17 +0100

    Set charset to utf-8

--- src/Templates/database_connect.inc.php
+++ src/Templates/database_connect.inc.php
@@ -2,6 +2,8 @@
 
     $mysqli = mysqli_connect('172.19.0.6', 'abizeitung','abizeitung','abizeitung');
 
+    mysqli_set_charset($mysqli, "utf8");
+
     if (!file_exists('/tmp/initiated')) {
         include 'Templates/init.inc.php';
         fopen('/tmp/initiated', 'w');
